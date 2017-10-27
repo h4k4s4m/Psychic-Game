665222cf0c96c2340faa58f0409470edae416b04
Sahm Samarghandi <h4k4s4m@gmail.com>
2017-10-27 12:41:48 -0700

    default

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+document.getElementById("reset").style.visibility = "hidden";
 $(document).ready(function () {
     var userKey = '';
     var userLives = 9;
@@ -8,26 +9,40 @@ $(document).ready(function () {
 
     //first generation of random character
     randomKey = charset.charAt(Math.floor(Math.random() * charset.length));
+    console.log(randomKey);
     document.onkeyup = function (event) {
 
         if (event.key == randomKey) {
-            alert("You guessed it!");
+            alert("You guessed it! The correct letter was " + event.key);
             userScore++;
+            document.getElementById("reset").style.visibility = "visible";
+            userLives = 9;
+            randomKey = charset.charAt(Math.floor(Math.random() * charset.length));
+            console.log(randomKey);
         }
         if (event.key != randomKey) {
             guesses += event.key;
             userLives--;
         }
         if (userLives < 1) {
+            document.getElementById("reset").style.visibility = "visible";
             alert("game over");
+            randomKey = charset.charAt(Math.floor(Math.random() * charset.length));
+            console.log(randomKey);
         }
 
         $("#player-score").html(userScore);
-        console.log(userScore);
         $("#lives-left").html(userLives);
-        console.log(userLives);
         $("#guesses-made").html(guesses);
-        console.log(guesses);
+
     }
-}
-);
+    $("#reset").click(function (event) {
+        userLives = 9;
+        userScore = 0;
+        guesses = "";
+        $("#player-score").html(userScore);
+        $("#lives-left").html(userLives);
+        $("#guesses-made").html(guesses);
+        document.getElementById("reset").style.visibility = "hidden";
+    })
+});
