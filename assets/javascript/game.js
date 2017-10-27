938839bb2d63f44683b951a39235ba7574e4ba63
Sahm Samarghandi <h4k4s4m@gmail.com>
2017-10-27 11:44:01 -0700

    default

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    var userKey = '';
+    var userLives = 9;
+    var userScore = 0;
+    var randomKey = '';
+    var guesses = "";
+    var charset = "abcdefghijklmnopqrstuvwxyz";
+
+    //first generation of random character
+    randomKey = charset.charAt(Math.floor(Math.random() * charset.length));
+    document.onkeyup = function (event) {
+
+        if (event.key == randomKey) {
+            alert("You guessed it!");
+            userScore++;
+        }
+        if (event.key != randomKey) {
+            guesses += event.key;
+            userLives--;
+        }
+        if (userLives < 1) {
+            alert("game over");
+        }
+    }
+}
+});
